38504933a213857fe9a6c555ec27ccd007ec327d
Isaac Urbina <7547113+isaacurbina@users.noreply.github.com>
2024-12-09 12:54:52 -0600

    208. How to Create Complex Designs and Layouts using SwiftUI

--- IsaacCard/ContentView.swift
+++ IsaacCard/ContentView.swift
@@ -9,16 +9,39 @@ import SwiftUI
 
 struct ContentView: View {
     var body: some View {
-        VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundStyle(.tint)
-            Text("Hello, world!")
+		ZStack {
+			Color(red: 0.282, green: 0.494, blue: 0.69) // #487eb0
+				.edgesIgnoringSafeArea(.all)
+			VStack {
+				Image("isaac")
+					.resizable()
+					.aspectRatio(contentMode: .fit)
+					.frame(width: 200, height: 200)
+					.clipShape(Circle())
+					.overlay(
+						Circle()
+							.stroke(
+								.white,
+								style: StrokeStyle(lineWidth: 5, lineCap: .round)
+							)
+					)
+				Text("Isaac Urbina")
+					.font(Font.custom("Glory", size: CGFloat(40)))
+					.bold()
+					.foregroundColor(.white)
+				Text("Android / iOS Developer")
+					.font(.system(size: 25))
+					.foregroundColor(.white)
+				Divider()
+				InfoView(text: "+1 (646) 552-7827", imageName:  "phone.fill")
+				InfoView(text: "isaacurbina89@gmail.com", imageName: "envelope.fill")
+			}
         }
-        .padding()
     }
 }
 
 #Preview {
     ContentView()
 }
+
+
